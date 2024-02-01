f1a605b1211779e9344e767da35a59076f7d7204
Jomay777 <joelmamani1020@gmail.com>
2024-02-01 00:21:40 -0300

    Routes protected  with roles

--- routes/web.php
+++ routes/web.php
@@ -44,60 +44,13 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
-Route::get('players/{player}/pdf', [PlayerController::class, 'pdf'])->name('players.pdf');
 
-Route::resource('/users', UserController::class);
-//Routes for clubs
-Route::resource('/clubs', ClubController::class);
-Route::get('clubs/{club}', [ClubController::class, 'show'])->name('clubs.show');
-
-//Routes for categories
-Route::resource('/categories', CategoryController::class);
-
-//Routes for tournaments
-Route::resource('/tournaments', TournamentController::class);
-Route::get('tournaments/{tournament}', [TournamentController::class, 'show'])->name('tournaments.show');
-
-//Routes for teams
-Route::resource('/teams', TeamController::class);
-Route::get('teams/{team}', [TeamController::class, 'show'])->name('teams.show');
-
-//Routes for players
-Route::resource('/players', PlayerController::class);
-Route::get('players/{player}', [PlayerController::class, 'show'])->name('players.show');
-
-
-//Routes for game_roles
-Route::resource('/game_roles', GameRoleController::class);
-Route::get('game_roles/{game_role}', [GameRoleController::class, 'show'])->name('game_roles.show');
-Route::get('game_roles/{game_role}/publish', [GameRoleController::class, 'publish'])->name('game_roles.publish');
-
-
-//Routes for game_schedulings
-Route::resource('/game_schedulings', GameSchedulingController::class);
-Route::get('game_schedulings/{game_scheduling}', [GameSchedulingController::class, 'show'])->name('game_schedulings.show');
-
-//Routes for games
-Route::resource('/games', GameController::class);
-Route::get('games/{game}', [GameController::class, 'show'])->name('games.show');
-
-//Routes for pass_requests
-Route::resource('/pass_requests', PassRequestController::class);
-Route::get('pass_requests/{pass_request}', [PassRequestController::class, 'show'])->name('pass_requests.show');
+Route::get('players/{player}/pdf', [PlayerController::class, 'pdf'])->name('players.pdf')->middleware('role:Administrador|Comité técnico|Delegado');
 
 //Routes for photo_players
 Route::resource('/photo_players', PhotoPlayerController::class);
 Route::get('photo_players/{photo_player}', [PhotoPlayerController::class, 'show'])->name('photo_players.show');
-//
-Route::resource('/roles', RoleController::class);
-Route::resource('/permissions', PermissionController::class);
 
-Route::delete('/roles/{role}/permissions/{permission}', RevokePermissionFromRoleController::class)
-    ->name('roles.permissions.destroy');
-Route::delete('/users/{user}/permissions/{permission}', RevokePermissionFromUserController::class)
-    ->name('users.permissions.destroy');
-Route::delete('/users/{user}/roles/{role}', RemoveRoleFromUserController::class)
-    ->name('users.roles.destroy');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
@@ -108,8 +61,58 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::middleware(['auth', 'role:admin'])->group(function () {
+Route::middleware(['auth', 'role:Administrador'])->prefix('/admin')->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+
+    Route::resource('/users', UserController::class);
+    Route::resource('/roles', RoleController::class);
+    Route::resource('/permissions', PermissionController::class);
+
+    Route::delete('/roles/{role}/permissions/{permission}', RevokePermissionFromRoleController::class)
+        ->name('roles.permissions.destroy');
+    Route::delete('/users/{user}/permissions/{permission}', RevokePermissionFromUserController::class)
+        ->name('users.permissions.destroy');
+    Route::delete('/users/{user}/roles/{role}', RemoveRoleFromUserController::class)
+        ->name('users.roles.destroy');       
 });
+Route::middleware(['auth', 'role:Administrador|Comité técnico|Directiva|Delegado'])->group(function () {
+
+    //Routes for clubs
+    Route::resource('/clubs', ClubController::class);
+    Route::get('clubs/{club}', [ClubController::class, 'show'])->name('clubs.show');
+    
+    //Routes for teams
+    Route::resource('/teams', TeamController::class);
+    Route::get('teams/{team}', [TeamController::class, 'show'])->name('teams.show');
+
+    //Routes for players
+    Route::resource('/players', PlayerController::class);
+    Route::get('players/{player}', [PlayerController::class, 'show'])->name('players.show');
+
+    //Routes for pass_requests
+    Route::resource('/pass_requests', PassRequestController::class);
+    Route::get('pass_requests/{pass_request}', [PassRequestController::class, 'show'])->name('pass_requests.show');    
+});
+
+Route::middleware(['auth', 'role:Administrador|Comité técnico|Directiva|Asistente'])->group(function () {
+    //Routes for tournaments
+    Route::resource('/tournaments', TournamentController::class);
+    Route::get('tournaments/{tournament}', [TournamentController::class, 'show'])->name('tournaments.show');
+
+    //Routes for game_roles
+    Route::resource('/game_roles', GameRoleController::class);
+    Route::get('game_roles/{game_role}', [GameRoleController::class, 'show'])->name('game_roles.show');
+    Route::get('game_roles/{game_role}/publish', [GameRoleController::class, 'publish'])->name('game_roles.publish');
+
+    //Routes for game_schedulings
+    Route::resource('/game_schedulings', GameSchedulingController::class);
+    Route::get('game_schedulings/{game_scheduling}', [GameSchedulingController::class, 'show'])->name('game_schedulings.show');
+
+    //Routes for games
+    Route::resource('/games', GameController::class);
+    Route::get('games/{game}', [GameController::class, 'show'])->name('games.show');
+});
+//Routes for categories
+Route::resource('/categories', CategoryController::class)->middleware('role:Administrador|Comité técnico|Directiva');
 
 require __DIR__.'/auth.php';
